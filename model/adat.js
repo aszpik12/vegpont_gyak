c1529b270fc57fbe73c3b17220697c635eb4db71
Petrovai Asztrik Örs <asztrikpetrovai@gmail.com>
2023-11-06 19:41:08 +0100

    tabla_megoldas

---
+++ model/adat.js
@@ -0,0 +1,20 @@
+export const urlapLeiro = {
+  nev: {
+    megj: "Név",
+    type: "text",
+    placeholder: "Valaki vagyok",
+    value: "",
+    regex: "[A-Z][a-z]{2,15}",
+  },
+  szul: {
+    megj: "Születési idő",
+    type: "number",
+    placeholder: "2000",
+    value: "2000",
+    regex: {
+      min: 1000,
+      max: 2023,
+    },
+    valid: "1000 és 2023 közötti számot írj!",
+  },
+};
--- view/TablaView.js
+++ view/TablaView.js
@@ -4,8 +4,9 @@ constructor(lista, szuloElem){
     let kiir = `<table>`;
     for (let i = 0; i < lista.length; i++) {
       kiir += `<tr>
-                <td>${lista[i].megj}</td>
-                <td>${lista[i].placeholder}</td>
+                <td>${lista[i].vnev}</td>
+                <td>${lista[i].knev}</td>
+                <td>${lista[i].szul}</td>
                 </tr>`;
     }
     kiir += `</table>`;
